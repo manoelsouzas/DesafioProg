d168c4da5458e0edb52af1f3d20558587dc99126
Manoel Souza <manoelsouzas@hotmail.com>
2019-10-14 23:14:54 -0300

    Update xadrex.py

--- xadrex.py
+++ xadrex.py
@@ -12,7 +12,7 @@ cavalo = 0
 torre = 0
 rainha = 0
 rei = 0
-for i in range (0,7):
+for i in range (0,8):
     peao = peao + tabuleiro[i].count(1)
     bispo = bispo + tabuleiro[i].count(2)
     cavalo = cavalo + tabuleiro[i].count(3)
